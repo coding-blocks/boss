a1e7d6cd0567429f07311541cd8990cfc5bbe022
Arnav Gupta <arnav@codingblocks.com>
2017-05-15 17:17:16 +0530

    fix type consts
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- db.js
+++ db.js
@@ -8,15 +8,15 @@ const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const Claim = sequelize.define('claim', {
     id: {
-        type: Sequelize.TYPE_INTEGER,
+        type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
     user: Sequelize.STRING,
-    issueUrl: Sequelize.TYPE_STRING,
-    pullUrl: {type: Sequelize.TYPE_STRING, unique: true},
-    repo: Sequelize.TYPE_STRING,
-    bounty: Sequelize.TYPE_INTEGER,
+    issueUrl: Sequelize.STRING,
+    pullUrl: {type: Sequelize.STRING, unique: true},
+    repo: Sequelize.STRING,
+    bounty: Sequelize.STRING,
     status: Sequelize.ENUM(Object.keys(config.CLAIM_STATUS).map((key) => config.CLAIM_STATUS[key]))
 });
 
