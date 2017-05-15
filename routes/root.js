4558625e94bcab012784ebc813d28c872b401a1e
Arnav Gupta <arnav@codingblocks.com>
2017-05-16 01:55:41 +0530

    leaderboard query
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- routes/root.js
+++ routes/root.js
@@ -1,12 +1,30 @@
 /**
  * Created by championswimmer on 16/05/17.
  */
+const Sequelize = require('sequelize');
 const Router = require('express').Router;
+
+const config = require('./../config');
+const db = require('./../utils/db');
 
 const route = new Router();
 
 route.get('/leaderboard', (req, res) => {
-    res.render('leaderboard')
+    db.Claim.findAll({
+        attributes: [
+            'user',
+            [Sequelize.fn('SUM', Sequelize.col('bounty')), 'bounty', 'DESC'],
+            [Sequelize.fn('COUNT', Sequelize.col('bounty')), 'pulls']
+        ],
+        group: 'user',
+        where: {
+            status: config.CLAIM_STATUS.ACCEPTED
+        }
+    }).then(results => {
+        res.render('leaderboard', {
+            userstats: results
+        })
+    })
 });
 
 exports = module.exports = route;
