e8023814c89be1a40b24bf167ca746f17f0d5691
Arnav Gupta <arnav@codingblocks.com>
2017-05-15 12:58:34 +0530

    initial commit
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by championswimmer on 15/05/17.
+ */
+const express = require('express');
+
+const app = express();
+
+
+
+app.listen(3232, function () {
+    console.log("Server started on http://localhost:3232");
+});
