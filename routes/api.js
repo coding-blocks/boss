a700a091c26ae62e731f10b4cbb7f9f4ffc24932
Arnav Gupta <dev@championswimmer.in>
2017-06-03 18:02:41 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ route.get('/claims', (req, res) => {
             res.send(data)
     }).catch(err => {
         console.log(err);
-        res.send("Sorry. Could not get the claims right now.");
+        res.status(500).send("Sorry. Could not get the claims right now.");
    });
     
 });
@@ -32,7 +32,7 @@ route.get('/claims/:id/delete', auth.adminOnly , (req, res) => {
         res.send({result: result})
     }).catch(err => {
         console.log(err);
-        res.send("Sorry. Could not delete the claim right now.");
+        res.status(500).send("Sorry. Could not delete the claim right now.");
     });
 });
 
@@ -42,7 +42,7 @@ route.get('/claims/:id/update', auth.adminOnly , (req, res) => {
         res.send({result: result})
     }).catch(err => {
         console.log(err);
-        res.send("Sorry. Could not update the claim right now.");
+        res.status(500).send("Sorry. Could not update the claim right now.");
     });
 });
 
