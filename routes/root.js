8b29fe210951a2ef04c7ce963812f7eb852d5992
nikhilhassija <nikhil.hassija@gmail.com>
2019-05-17 08:31:37 +0530

    Fixed NaN values on stats page

--- routes/root.js
+++ routes/root.js
@@ -91,8 +91,8 @@ route.get('/stats', (req, res) => {
       res.render('pages/stats', {
         participants: data[0],
         claims: data[1],
-        accepted: data[2],
-        totalclaimed: data[3],
+        accepted: data[2] || 0,
+        totalclaimed: data[3] || 0,
         menu: { stats: 'active' }
       })
     })
